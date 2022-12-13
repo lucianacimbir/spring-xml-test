26f6e5c5e45ba84134b8b1f61631a19712dd78b7
lucianacimbir <lucianacimbir@gmail.com>
2022-12-13 15:51:28 +0200

    Initialize spring boot project

---
+++ project/src/main/java/com/example/project/SpringXmLprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringXmLprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringXmLprojectApplication.class, args);
+	}
+
+}
